ca60756091dd29d66f452cedde37deaf501bf2d1
25shpy <modianos@msn.com>
2026-07-26 17:37:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const C='agenda-v4-cache-1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='agenda-v5-cache-1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
- if(e.request.url.includes('gstatic.com')||e.request.url.includes('googleapis.com')) return;
- e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
+  if(e.request.url.includes('gstatic.com')||e.request.url.includes('googleapis.com')) return;
+  e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));
 });
