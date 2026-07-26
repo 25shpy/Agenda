a3e14646cd0662e2191bea407553fda7d10bef8a
25shpy <modianos@msn.com>
2026-07-26 17:46:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='agenda-v6-cache-1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='agenda-v7-cache-1',A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
